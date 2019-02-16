8601d4ae0fc93e242c26d38024d7fe8ef762039c
LucasCFerraz <lucascfpaolillo@gmail.com>
2019-02-16 15:35:12 -0200

    Add mapNetworkError to calls

--- data/src/main/java/com/nuvem/data/weather/WeatherRepository.kt
+++ data/src/main/java/com/nuvem/data/weather/WeatherRepository.kt
@@ -2,6 +2,7 @@ package com.nuvem.data.weather
 
 import com.nuvem.api_client.weather.WeatherClient
 import com.nuvem.data.weather.converter.toDomain
+import com.nuvem.data.weather.error.mapNetworkError
 import com.nuvem.domain.weather.Response
 import com.nuvem.domain.weather.model.Weather
 import com.nuvem.domain.weather.WeatherRepositoryContract
@@ -14,9 +15,11 @@ class WeatherRepository @Inject constructor(private val clientWeather: WeatherCl
 
     override fun getForecast(city: String): Single<Response<WeatherForecast>> =
         clientWeather.getForecast(city, "2cebe458561b8e39965d632395a2852e")
+            .mapNetworkError()
             .map { Response.success(it.toDomain()) }
 
     override fun getWeatherFromCity(city: String): Single<Response<Weather>> =
         clientWeather.getCurrentWeather(city, "2cebe458561b8e39965d632395a2852e")
+            .mapNetworkError()
             .map { Response.success(it.toDomain()) }
 }
